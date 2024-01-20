ab798fdb7e51dafc64eaef473bb561752fc04128
smazepaa <sof.mazepa@gmail.com>
2024-01-20 13:20:37 +0200

    added default get (/)

--- task2/server.js
+++ task2/server.js
@@ -3,6 +3,10 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+app.get('/', (req, res) => {
+    res.send('This is the default page');
+});
+
 app.get('/endpoint', (req, res) => {
     res.json({ message: "This is an endpoint for HW1" });
 });
